57f12d9a21ba5045733de285577814db03ad0848
yanqiangmiffy <1185918903@qq.com>
2023-04-19 23:03:07 +0800

    feature@创建hf应用

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from clc.langchain_application import LangChainApplication
 # 修改成自己的配置！！！
 class LangChainCFG:
     llm_model_name = 'THUDM/chatglm-6b-int4-qe'  # 本地模型文件 or huggingface远程仓库
-    embedding_model_name = 'GanymedeNil/text2vec-large-chinese'  # 检索模型文件 or huggingface远程仓库
+    embedding_model_name = 'GanymedeNil/text2vec-base-chinese'  # 检索模型文件 or huggingface远程仓库
     vector_store_path = './cache'
     docs_path = './docs'
     kg_vector_stores = {
